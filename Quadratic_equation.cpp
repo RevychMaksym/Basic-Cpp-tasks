1d035b6e68a1325ce3034478f857262c8513443a
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-17 11:02:01 +0300

    Update Quadratic_equation.cpp

--- Quadratic_equation.cpp
+++ Quadratic_equation.cpp
@@ -3,34 +3,38 @@
 #include <limits>
 
 int main(int argc, char const *argv[]) {
-  constexpr int MAX = std::numeric_limits<short>::max();
-  constexpr int MIN = std::numeric_limits<short>::min();
-  double a;
-  std::cout << "Please enter your A, B, C considering the range [" << MIN << ","
-            << MAX << "]:\nA:";
+  constexpr int MAX = std::numeric_limits<int>::max();
+  constexpr int MIN = std::numeric_limits<int>::min();
+  std::cout << "Program for solving quadratic equations (Ax^2 + Bx + C = 0): "
+            << "\nPlease enter your A, B, C considering the range [" << MIN
+            << "," << MAX << "]:\nA:";
+  long a;
   std::cin >> a;
   if (a != 0 && a >= MIN && a <= MAX) {
     std::cout << "B:";
-    double b;
+    long b;
     std::cin >> b;
     if (b >= MIN && b <= MAX) {
       std::cout << "C:";
-      double c;
+      long c;
       std::cin >> c;
       if (c >= MIN && c <= MAX) {
-        auto discriminant = std::pow(b, 2) - (4 * a * c);
+        const auto FACTOR_4 = 4;
+        const auto FACTOR_2 = 2;
+        const auto discriminant = b * b - (FACTOR_4 * a * c);
         if (discriminant > 0) {
           std::cout << "Discriminant > 0, your example has 2 solutions.\n";
-          auto sqrt_discriminant = sqrt(discriminant);
-          auto x1 = (-b - sqrt_discriminant) / (2 * a);
-          auto x2 = (-b + sqrt_discriminant) / (2 * a);
-          std::cout << "x1: " << x1 << "\nx2: " << x2;
+          const auto sqrt_discriminant = sqrt(discriminant);
+          const auto X1 = (-b - sqrt_discriminant) / (FACTOR_2 * a);
+          const auto X2 = (-b + sqrt_discriminant) / (FACTOR_2 * a);
+          std::cout << "x1: " << X1 << "\nx2: " << X2;
         } else if (discriminant == 0) {
           std::cout << "Discriminant = 0, your example has 1 solution.\n";
-          auto x = (-b) / (2 * a);
-          std::cout << "x: " << x;
+          const auto X = (-b) / (2 * a);
+          std::cout << "x: " << X;
         } else if (discriminant < 0) {
-          std::cout << "Discriminant is negative, your example has no real solutions";
+          std::cout
+              << "Discriminant is negative, your example has no real solutions";
         }
       } else {
         std::cout << "Number C is out of range, please try again\n";
