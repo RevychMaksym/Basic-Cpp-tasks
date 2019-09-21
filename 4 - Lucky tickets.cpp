c1cdf02aeff12083ad0fb3bbe0e0751bd7d5f744
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-21 20:51:47 +0300

    Fourth task completed
    Program checking if your ticket is lucky.

---
+++ 4 - Lucky tickets.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <limits>
+
+bool isLucky(int input) {
+  int input_length = 0;
+  int argument = input;
+
+  int counter = 0;
+  while (input) {
+    counter++;
+    input_length += input % 10;
+    input /= 10;
+  }
+
+  int sum1 = 0;
+  if (input_length % 2 != 0) {
+    std::cout << "Your number is odd, please try again!" << std::endl;
+    return false;
+  }
+
+  for (int i = 0; i < counter / 2; i++) {
+    sum1 += argument % 10;
+    argument /= 10;
+  }
+
+  int sum2 = 0;
+  for (int i = 0; i < counter / 2; i++) {
+    sum2 += argument % 10;
+    argument /= 10;
+  }
+  return sum2 == sum1 ? true : false;
+}
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Please enter the number of your ticket to find your luck: ";
+  int input;
+  std::cin >> input;
+  bool res = isLucky(input);
+  if (res == true) {
+    std::cout << "Congrats, You have a winning ticket. You won 9999653$!"
+              << std::endl;
+  } else {
+    std::cout << "Unlucky, better luck next time!" << std::endl;
+  }
+  return 0;
+}
