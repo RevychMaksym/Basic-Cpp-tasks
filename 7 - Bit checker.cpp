d8878eceaf62ec8f992b1b716416d4358dafa381
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-22 23:35:00 +0300

    Seventh task completed
    Another bit checker for any set position in number's bit represantation

--- 7 - Bit checker.cpp
+++ 7 - Bit checker.cpp
@@ -7,17 +7,15 @@ int main(int argc, char const *argv[]) {
   std::cin >> input;
   constexpr uint32_t MAX = std::numeric_limits<uint32_t>::max();
   if (MAX > (input + 1)) {
-
     struct BitChecker {
-      unsigned int bit : 6;
+      uint8_t bit : 5;
     };
-
     unsigned int is_set;
     BitChecker bit_number;
     std::cout << "Enter the bit you want to check (0-32): ";
     std::cin >> is_set;
     bit_number.bit = is_set;
-    if (is_set <= 32 && is_set != 0) {
+    if (is_set <= 32 && is_set > 0) {
       const bool res = input & (1 << (bit_number.bit));
       if (res == true) {
         std::cout << "Yes" << std::endl;
