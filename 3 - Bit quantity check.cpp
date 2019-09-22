4ffcceff99c510f3450221548bd88d62589f301d
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-22 23:31:56 +0300

    Third task completed
    Manual bit checker

--- 3 - Bit quantity check.cpp
+++ 3 - Bit quantity check.cpp
@@ -1,16 +1,16 @@
-#include <bitset>
 #include <iostream>
-#include <limits>
 
 int main(int argc, char const *argv[]) {
   std::cout << "Please enter number to learn its binary representation and "
                "quantity of 1 in it: ";
   int input;
   std::cin >> input;
-
-  std::cout << "The binary represantation of your number - "
-            << std::bitset<16>(input) << std::endl;
-  std::bitset<std::numeric_limits<int>::digits> bitset(input);
-  std::cout << "Your input has " << bitset.count() << " ones." << std::endl;
+  unsigned int quantity_1;
+  for (unsigned int i = 0; i < sizeof(int) * 8; i++) {
+    if (input & (1 << i)) {
+      quantity_1++;
+    }
+  }
+  std::cout << "Your input has " << quantity_1 << " ones." << std::endl;
   return 0;
 }
