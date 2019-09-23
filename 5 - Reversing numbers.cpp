4c775b02f8c814dd9c4bd8e74046bc91184f7e6e
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-23 23:15:15 +0300

    Task5: Remarks corrected

--- 5 - Reversing numbers.cpp
+++ 5 - Reversing numbers.cpp
@@ -4,13 +4,12 @@ int main(int argc, char const *argv[]) {
   std::cout << "Please enter the number you want to reverse: ";
   int32_t input;
   std::cin >> input;
-  int reverse;
-  while(input != 0)    
-  {    
-    rem = input % 10;      
-    reverse = reverse * 10 + rem;    
-     input /= 10;    
-  }    
- std::cout<<"Reversed Number: "<< reverse <<std::endl;     
-return 0;  
-}  
+  int32_t reverse = 0;
+  while (input != 0) {
+    rem = input % 10;
+    reverse = reverse * 10 + rem;
+    input /= 10;
+  }
+  std::cout << "Reversed Number: " << reverse << std::endl;
+  return 0;
+}
