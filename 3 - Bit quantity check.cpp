0c22734398a0f39d0abc47b3f785694cc18219a7
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-23 22:58:56 +0300

    Task3: Remarks corrected

--- 3 - Bit quantity check.cpp
+++ 3 - Bit quantity check.cpp
@@ -3,13 +3,11 @@
 int main(int argc, char const *argv[]) {
   std::cout << "Please enter number to learn its binary representation and "
                "quantity of 1 in it: ";
-  int input;
+  unsigned int input;
   std::cin >> input;
-  unsigned int quantity_1;
-  for (unsigned int i = 0; i < sizeof(int) * 8; i++) {
-    if (input & (1 << i)) {
-      quantity_1++;
-    }
+  unsigned int quantity_1 = 0;
+  for (unsigned int i = 0; i < sizeof(int) * 8; i++) { 
+    quantity_1 += input & (1 << i);  
   }
   std::cout << "Your input has " << quantity_1 << " ones." << std::endl;
   return 0;
