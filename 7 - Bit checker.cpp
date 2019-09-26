466666d8b05274b549f6372ebc04bbeb409f157f
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-26 15:40:22 +0300

    Task7: remarks rerecorrected

--- 7 - Bit checker.cpp
+++ 7 - Bit checker.cpp
@@ -2,23 +2,22 @@
 #include <limits>
 
 int main(int argc, char const *argv[]) {
-  std::cout << "Please enter your number to check if any bit is set in it: ";
-  uint32_t input;
+  std::cout << "Please enter your x32 number to check if any bit is set in it: ";
+  uint64_t input;
   std::cin >> input;
   constexpr uint32_t MAX = std::numeric_limits<uint32_t>::max();
   if (MAX >= (input) && 0 <= (input)) {
-
+    std::cout << "Enter the bit you want to check: ";
     uint32_t check_bit;
     std::cin >> check_bit;
-
-    if (check_bit < 1 || check_bit > 32) {
+    constexpr unsigned int MIN_EL = 1;
+    constexpr unsigned int MAX_EL = 32;
+    if (MIN_EL > check_bit || MAX_EL < check_bit ) {
       std::cout << "Sorry you have entered incorrect number of 32th bit value. "
                    "Please try again!";
       return 1;
     }
-    std::cout << "Bit is";
-    input &(1 << (check_bit - 1)) ? std::cout << " set  \n"
-                                  : std::cout << " not set \n";
+    std::cout << "Bit is" << (input &(1 << (check_bit - 1)) ? " set  \n" : " not set \n");
   } else {
     std::cout << "Number overflow!" << std::endl;
   }
