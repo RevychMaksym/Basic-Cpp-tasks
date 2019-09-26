5fb89e10bdc9ddd058e8dd6d0434311c310161c0
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-26 14:08:44 +0300

    Task3: Postincrement changed

--- 3 - Bit quantity check.cpp
+++ 3 - Bit quantity check.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[]) {
   unsigned int input;
   std::cin >> input;
   unsigned int quantity_1 = 0;
-  for (unsigned int i = 0; i < sizeof(int) * 8; i++) { 
+  for (unsigned int i = 0; i < sizeof(int) * 8; ++i) { 
     quantity_1 += input & (1 << i);  
   }
   std::cout << "Your input has " << quantity_1 << " ones." << std::endl;
