67a525f76ebe3e836cfaa9a1f4ccc5495a828456
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-09-26 15:49:13 +0300

    Task1: Completed

--- 1 - is_digit_function.cpp
+++ 1 - is_digit_function.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[]) {
   char mass_char[SIZE]{};
   std::cin >> mass_char;
   std::cout << "Your string: " << mass_char << std::endl;
-  for (size_t i = 0; i < SIZE - 1; ++i) {
+  for (size_t i = 0; i < SIZE; ++i) {
     if ((mass_char[i] <= '9') && (mass_char[i] >= '0')) {
       std::cout << "Is digit - " << mass_char[i] << "; ";
     }
