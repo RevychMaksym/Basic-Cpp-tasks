47e29c292c2ce1aa980f2e82da42d5503db9377d
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-10-01 11:36:50 +0300

    Task3 - recorrected

--- 3 - To_lowercase.cpp
+++ 3 - To_lowercase.cpp
@@ -7,10 +7,10 @@ int main(int argc, char const *argv[]) {
   char mass_char[SIZE]{};
   std::cin >> mass_char;
   std::cout << "Your string: " << mass_char << std::endl;
-  
+  const unsigned short ASCII_COUNTER = 32;
   for (size_t i = 0; i < SIZE; ++i) {
     if ((mass_char[i] >= 'A') && (mass_char[i] <= 'Z')) {
-      mass_char[i] += 32;
+      mass_char[i] += ASCII_COUNTER;
     }
   }
   std::cout << "Your string after: " << mass_char << std::endl;
